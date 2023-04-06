aaf9f16769a5237643add80373861268f999a69d
souharebai111@gmail.com <souharebai111@gmail.com>
2023-04-06 22:56:21 +0100

    deploy contract

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork : 'goerli',
+    networks : {
+      hardhat : {},
+      localhost: {
+        url: "http://localhost:8545",
+        chainId: 31337
+      },
+      goerli:{
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
